d7a5ac13563409a723b5740c7fb86da1a0a683ca
tenzin-dawa <76651242+tenzin-dawa@users.noreply.github.com>
2022-02-01 18:21:08 +0530

    Add files via upload

---
+++ Ass-3/3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace mainprogram
+{
+    class Fraction
+    {
+        public static void Main()
+        {
+            double a;
+            Console.WriteLine("Please enter the numerator ?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the denumerator ?");
+            int denum = Convert.ToInt32(Console.ReadLine());
+            if(denum==0)
+            {
+                throw new DivideByZeroException();
+            }
+            intdiv(num,denum);
+            a = floatdiv(num,denum);
+            Console.WriteLine("Floating point division result = " +a);
+            mixedfraction(num,denum);
+        }
+        public static void intdiv(int x,int y)
+        {
+            Console.WriteLine($"Integer division result = {(int)x/y} with a remainder {x%y}");
+        }
+        public static double floatdiv(int x,int y)
+        {
+            return (float)x/y;
+        }
+        public static void mixedfraction(int x,int y)
+        {
+            int Num = (int)x/y;
+            Console.WriteLine($"The result as a mixed fraction is = {Num} {x%y}/{y}");
+        }
+    }
+}
+
---
+++ Ass-3/3.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class StringOperation 
+{
+    static void Main()
+    {
+        Console.WriteLine("\nPlease enter a String to find length :");
+        string? Str = Console.ReadLine();
+        Console.WriteLine("The String is :"+Str);
+        Console.WriteLine("The length of the String is :"+Str?.Length);
+
+        Console.WriteLine("\nPlease enter a sentence to Identify it's type :");
+        string Sentence = Console.ReadLine();
+
+        if( isDeclarative(Sentence) )
+        {
+            Console.WriteLine("It is Declarative Sentence!!!");
+        }
+        else if(isInterrogatory(Sentence))
+        {
+            Console.WriteLine("It is Interrogatory Sentence!!!");
+        }
+        else if(isExclamatory(Sentence))
+        {
+            Console.WriteLine("It is Exclamatory Sentence!!!");
+        }
+        else
+        {
+            Console.WriteLine("It is Not a Sentence!!!");
+        }
+
+        Console.WriteLine("\nPlease enter your Full Name :");
+        string fName = Console.ReadLine();
+        Change(fName);
+    }
+    public static bool isDeclarative(string s)
+    {
+        return s[s.Length-1]=='.';
+    }
+     public static bool isInterrogatory(string s)
+    {
+        return s[s.Length-1]=='?';
+    }
+     public static bool isExclamatory(string s)
+    {
+        return s[s.Length-1]=='!';
+    }
+    public static void Change(string s)
+    {
+         string[] splitName = s.Split(' ');
+        if(splitName.Length==1)
+            Console.WriteLine("Your Name is : "+splitName[0]);
+        else
+            Console.WriteLine("Your Name is : "+splitName[1]+", "+splitName[0]);
+    }
+}
